453063aa53e5321c65316f7705e1e1342fa96094
Inno-Kali <innocentkidk96@gmail.com>
2019-07-16 15:55:02 +0000

    IoT project

--- fib.py
+++ fib.py
@@ -1,7 +1,16 @@
 def fib_rec(n):
-    if n == 1:
-        return 1
+    if n <= 1:
+        return n
     else:
-        return n * fib_rec(n-1) -( n * n*fib_rec(n-2) )
-    
-print(fib_rec(4))
+        return (fib_rec(n-1)) + (fib_rec(n-2))
+                                 
+                        
+                        
+                        
+nterm = int(input("Enter: "))
+
+for x in range(nterm):
+    #print(fib_rec(x))
+    print(list(map(fib_rec, range(1,11))))
+            
+
